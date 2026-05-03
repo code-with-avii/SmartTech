8af4e30a63ad217bcf949cb22786c6442ea0f6b3
code-with-avii <aviimondal689@gmail.com>
2026-05-03 23:57:52 +0530

    update

--- client/app/src/Products/Mobilesection.jsx
+++ client/app/src/Products/Mobilesection.jsx
@@ -12,7 +12,7 @@ const MobileSection = () => {
       <div className="container mx-auto px-4 py-8">
         <MobilesHero />
       </div>
-      <ProductSection type="mobile" title="Mobile Phones" />
+      <Productsection type="mobile" title="Mobile Phones" />
       <Footer />
     </div>
   );
