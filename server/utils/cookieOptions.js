8585712cd80dcb44d255ed12617f39ea78d4ca38
Avii <aviimondal689@gmail.com>
2026-08-08 16:04:19 +0530

    added rate-limit

--- server/utils/cookieOptions.js
+++ server/utils/cookieOptions.js
@@ -1,4 +1,4 @@
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production" || process.env.NODE_ENV !== "development";
 
 export const accessCookieOptions = {
   httpOnly: true,
